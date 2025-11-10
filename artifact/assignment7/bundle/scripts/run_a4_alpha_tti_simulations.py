883f7d14dce4cd1597e814c16ff2372560cb0c1c
Behab Patnaik <behab.patnaik@gmail.com>
2025-11-10 01:00:50 -0800

    Added scripts to run cache size simulations, alpha tti simulations, cachse size simulations, protected cap sims, tau dt simus

---
+++ artifact/assignment7/bundle/scripts/run_a4_alpha_tti_simulations.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from .utils import run_python_script
+
+
+def main() -> None:
+    run_python_script("assignment4/scripts/simulation/run_figure_7_simulations.py")
+
+
+if __name__ == "__main__":
+    main()
+
