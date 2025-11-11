d372dd0b166a2ca9ca0aa425771f332af0e27701
Behab Patnaik <behab.patnaik@gmail.com>
2025-11-11 13:17:19 -0800

    Added tau_DT ablation simulation, Added protected capacity simulations, Added alpha_TTI adaptations simulations

---
+++ artifact/assignment7/bundle/scripts/run_a5_alpha_tti_simulations.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from .utils import run_python_script
+
+
+def main() -> None:
+    run_python_script("assignment5/scripts/simulation/run_fig4_alpha_tti.py")
+
+
+if __name__ == "__main__":
+    main()
+
